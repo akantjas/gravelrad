d8afe6fda1aa13b3dc409dcd7477b8dcb396f6a8
Alexandre Kantjas <alexandre.kantjas@gmail.com>
2020-08-11 09:09:22 +0200

    Seed file and Bike views

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,2 +1,46 @@
 class BikesController < ApplicationController
+    before_action :set_bike, only: [:show, :edit, :update, :destroy] 
+
+    def index
+        @bikes = Bike.all
+    end
+
+    def compare
+        @bikes = Bike.where(rating: 5)
+    end 
+
+    def show
+    end
+
+    def new
+        @bike = Bike.new
+    end
+
+    def create
+        @bike = Bike.new(bike_params)
+        @bike.save
+        redirect_to bike_path(@bike)
+    end
+
+    def edit
+    end
+
+    def update
+        @bike.update(bike_params)
+        redirect_to bike_path(@bike)
+    end
+
+    def destroy
+        @bike.destroy
+        redirect_to bikes_path
+    end
+
+    private
+    def set_bike
+        @bike = Bike.find(params[:id])
+    end
+    
+    def bike_params
+        params.require(:bike).permit(:name, :rating, :price, :vendor)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning up database..."
+Bike.destroy_all
+puts "Database cleaned"
+
+puts "Creating bikes..."
+100.times do
+    bike = Bike.create(
+    name: Faker::Sports::Football.player,
+    brand: Faker::Sports::Football.team
+    )
+    puts "#{bike.id} created"
+end
