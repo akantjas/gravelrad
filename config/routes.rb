5ef9129bcacca8011680725ec01e0b239ab120de
Alexandre Kantjas <alexandre.kantjas@gmail.com>
2020-08-20 22:31:50 +0200

    Changed root path to bikes#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root to: 'bikes#index'
+
   resources :bikes do
     collection do
       get '/compare', to: 'bikes#compare', as: :compare
