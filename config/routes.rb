9163612939bd7edfc9cad3af10988224d248f4ba
Alexandre Kantjas <alexandre.kantjas@gmail.com>
2020-08-20 22:52:05 +0200

    Don't display actions possible for logged in users when user_signed_in? is false

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_scope :user do
+    get "signup", to: "devise/registrations#new"
+    get "login", to: "devise/sessions#new"
+    get "logout", to: "devise/sessions#destroy"
+  end
 
   root to: 'bikes#index'
 
