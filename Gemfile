d9f45b06a477a2e3379034be8a2ad09cb2a6c3e3
Alexandre Kantjas <alexandre.kantjas@gmail.com>
2020-08-20 22:27:16 +0200

    Added Devise

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'cloudinary', '~> 1.12.0'
 
 gem 'pg_search', '~> 2.3.0'
 
+gem 'devise'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
   resources :bikes do
     collection do
       get '/compare', to: 'bikes#compare', as: :compare
     end
-  end
+  end 
 end
